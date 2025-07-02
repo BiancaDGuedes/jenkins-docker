84b26a5d244b2cc5f268978d04db924f19074c57
ErivandsonMelo <warmlake636@gmail.com>
2025-07-02 16:54:14 -0400

    Retornando aos testes funcionais

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,12 +1,15 @@
 import unittest
-import converter
+from app.converter import fahrenheit_to_celsius, celsius_to_fahrenheit
 
-class TestConversao(unittest.TestCase):
+class TestConverter(unittest.TestCase):
+
     def test_fahrenheit_to_celsius(self):
-        self.assertEqual(converter.fahrenheit_to_celsius(32), 0)
+        self.assertAlmostEqual(fahrenheit_to_celsius(32), 0)
+        self.assertAlmostEqual(fahrenheit_to_celsius(212), 100)
 
-    def test_fail_proposital(self):
-        self.assertEqual(1, 2)  # <-- Vai falhar de propósito
+    def test_celsius_to_fahrenheit(self):
+        self.assertAlmostEqual(celsius_to_fahrenheit(0), 32)
+        self.assertAlmostEqual(celsius_to_fahrenheit(100), 212)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
